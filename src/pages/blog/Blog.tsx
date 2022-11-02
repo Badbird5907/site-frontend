00c74e3b18f52ceb73448b6e3596e3447b243971
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2022-11-02 15:13:50 -0400

    massive changes

--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -1,6 +1,10 @@
 function Blog() {
     return (
         <div className={"app"}>
+            <h1 className={"centered"}>Blog</h1>
+            <section id="main">
+
+            </section>
         </div>
     )
 }
