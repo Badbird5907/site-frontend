196dc9e687e8ba5c428f793fbfc904c6d7f18d7f
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2022-11-02 12:01:12 -0400

    use import.meta.env.PROD

--- src/services/APIService.ts
+++ src/services/APIService.ts
@@ -1,1 +1,2 @@
-export const backendURL = import.meta.env.BACKEND_URL || 'http://localhost:8080';
+//export const backendURL = import.meta.env.BACKEND_URL || 'http://localhost:8080';
+export const backendURL = (import.meta.env.PROD ? 'https://backend.badbird.dev/' : 'http://localhost:8080');
