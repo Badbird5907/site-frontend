96d6529e512a4d69787ae4f054c9e7f22295cf1c
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2022-11-14 10:19:09 -0500

    enable experimental edge runtime

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 import * as removeImports from "next-remove-imports";
 
 export default removeImports.default({
-
+    experimental: {
+        runtime: 'experimental-edge',
+    },
+    reactStrictMode: true,
+    swcMinify: true
 });
