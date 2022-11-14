bc05b181ae2cd81520beaa76ddc7322074b4e11f
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2022-11-14 14:58:56 -0500

    add react-typewriter back

--- src/components/pages/main/Home.tsx
+++ src/components/pages/main/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Typewriter from "typewriter-effect";
 import {Button} from "@mui/material";
 //import {Button, Stack} from "@mui/material";
 import styles from './home.module.css'
@@ -13,8 +14,7 @@ const Home = () => {
                     <h3>But people call me Bad. <i>sometimes</i></h3>
                     <h3>I'm a:</h3>
                     <h3>
-                        {/*
-     <Typewriter
+                        <Typewriter
                             options={{
                                 strings: ['Full Stack Developer', 'Freelancer', 'Student', 'Video Editor'],
                                 autoStart: true,
@@ -22,7 +22,6 @@ const Home = () => {
                                 delay: 70,
                             }}
                         />
-                        */}
                     </h3>
                     {/*
   <div className={"centered"}>
