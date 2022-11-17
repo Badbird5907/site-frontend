dfaea11ff3e3243aee97d837ebc366c9e4995ab3
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2022-11-16 22:04:21 -0500

    fix build again

--- src/components/twitter/Styles.tsx
+++ src/components/twitter/Styles.tsx
@@ -3,22 +3,22 @@
 import React from "react";
 import styles from "../../styles/components/Tweet.module.css"
 // Rewrite this to use plain react and MUI
-export const TweetWrapper = ({children}) => {
+export const TweetWrapper = ({children}: any) => {
     return <div className={styles.tweetWrapper}>{children}</div>
 }
 export const Name = ({children, ...props}) => <a className={styles.name} {...props}>{children}</a>;
 
-export const ImageGrid = ({children}) => {
+export const ImageGrid = ({children}: any) => {
     return <div className={styles.imageGrid}>{children}</div>
 }
 
-export const SingleImageWrapper = ({children}) => {
+export const SingleImageWrapper = ({children}: any) => {
     return <div className={styles.singleImageWrapper}>{children}</div>
 }
 
 
-export const ActionIcons = ({children, ...props}) => {
+export const ActionIcons = ({children, ...props}: any) => {
     return <div className={styles.actionIcons} {...props}>{children}</div>
 }
 
-export const singleImage = styles.singleImage;
+export const singleImage: any = styles.singleImage;
