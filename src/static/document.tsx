206a548dc29ea5e37481fbc316fc0e31be9c8f55
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2022-11-30 15:18:27 -0500

    set icon

---
+++ src/static/document.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+import React from 'react';
+
+const Page = (props) => (
+    <div>
+        <Head>
+            <link rel="icon" type="image/gif" href="https://cdn.badbird.dev/assets/profile.gif"/>
+        </Head>
+    </div>
+);
+
+export default Page;
