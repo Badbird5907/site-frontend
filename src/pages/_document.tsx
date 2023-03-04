4b38fc823047cc9b98dcd29261df068f7deb1e54
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-03-04 00:07:24 -0500

    Add clarity

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,6 +2,7 @@
 
 import Document, {Head, Html, Main, NextScript} from "next/document";
 import React from "react";
+import Script from "next/script";
 
 class MyDocument extends Document {
     render() {
@@ -9,6 +10,7 @@ class MyDocument extends Document {
             <Html lang={'en'}>
                 <Head>
                     <link rel="icon" type="image/gif" href="https://cdn.badbird.dev/assets/profile.gif"/>
+                    <script src={"/assets/js/clarity.js"} />
                 </Head>
                 <body>
                 <Main/>
